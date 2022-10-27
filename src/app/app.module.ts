9bc892a2c5e0ce3a2a1581a3768aafde3a27b809
Agustin Mellado <aguxmellado@gmail.com>
2022-10-27 17:16:23 -0300

    correccion de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { PokedexComponent } from './components/Home/pokedex/pokedex.component';
-import { PagesComponent } from './components/pages/pages.component';
+import { PokedexComponent } from './components/pages/pokedex/pokedex.component';
+
 import { CounterPickComponent } from './components/pages/counter-pick/counter-pick.component';
 import { CuriosidadesComponent } from './components/pages/curiosidades/curiosidades.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,10 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NavBarComponent,
     PokedexComponent,
-    PagesComponent,
     CounterPickComponent,
     CuriosidadesComponent
   ],
