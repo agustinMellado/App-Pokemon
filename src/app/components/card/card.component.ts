d828b79f426168890ab6d2d19a9a7a965b417871
Javier Salinas <alejandrosalinas.pacha@gmail.com>
2022-11-01 21:48:12 -0300

    prueba card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  pokemons=[{
+    nombre:"Vaporeon",
+    descripcion:"Vaporeon es un Pokémon de tipo agua introducido en la primera generación. Es una de las ocho Eeveeluciones, es decir, las posibles evoluciones de Eevee.",
+    url:"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fc/latest/20210627185640/Vaporeon.png/1200px-Vaporeon.png"
+  }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
